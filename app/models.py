5fab0880e8f9df6e3082765da48b5d77d0f5f30b
telmoesnaola <tesnaola001@ikasle.ehu.eus>
2022-05-04 13:03:52 +0200

    commit inicial

---
+++ app/models.py
@@ -0,0 +1,27 @@
+"""
+Definition of models.
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+
+class Pelicula(models.Model):
+	titulo = models.CharField(max_length=100)
+	direccion = models.CharField(max_length=60)
+	anio = models.IntegerField(validators=[MaxValueValidator(99999999999)])
+	genero = models.CharField(max_length=60)
+	sinopsis = models.CharField(max_length=700)
+	votos = models.IntegerField(validators=[MaxValueValidator(99999999999)])
+	imagen = models.ImageField(upload_to="images/", default="notfound.jpg")
+
+	def __unicode__(self):
+		return self.titulo
+
+class Critico(models.Model):
+	usuario_id = models.ForeignKey(User,on_delete=models.CASCADE)
+	favoritas = models.ManyToManyField(Pelicula)
+
+	def __unicode__(self):
+		return unicode(self.usuario_id)
+
